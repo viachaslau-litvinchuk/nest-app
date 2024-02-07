00a528cbcf207250e2dd988cb8f5043f1617f715
viachaslau-litvinchuk <viacaslau@gmail.com>
2024-02-07 22:22:03 +0300

    Substituted npm package to create csv

--- src/app.model.ts
+++ src/app.model.ts
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line prettier/prettier
 export class SpaceUsers {
   space: string;
   user: string;
 }
 
 export class UserRoles {
-    user: string;
-    role: string;
+  user: string;
+  role: string;
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,101 +1,111 @@
-/* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { promisify } from 'util';
 import { exec as execNonPromise } from 'child_process';
 import { SpaceUsers, UserRoles } from './app.model';
+import { createObjectCsvWriter } from 'csv-writer';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ObjectsToCsv = require('objects-to-csv');
 const exec = promisify(execNonPromise);
 
-// ADD TIMESTAMP in resultset
 @Injectable()
 export class AppService {
-
   async getSpacesList(): Promise<string[]> {
     try {
-        const { stdout } = await exec('datasphere spaces list');
+      const { stdout } = await exec('datasphere spaces list');
 
-        return JSON.parse(stdout);    
-    } catch(err) {
-        Logger.error(`Error: ${err}`);
+      return JSON.parse(stdout);
+    } catch (err) {
+      Logger.error(`Error: ${err}`);
     }
   }
 
- async  getSpacesUsers(spaces: string[]): Promise<SpaceUsers[]> {
+  async getSpacesUsers(spaces: string[]): Promise<SpaceUsers[]> {
     try {
-        const currentTimestamp = new Date(Date.now()).toISOString();
+      const currentTimestamp = new Date(Date.now()).toISOString();
 
-        const spacesUsers = await Promise.all(
+      const spacesUsers = await Promise.all(
         spaces.map(async (space) => {
-            const { stdout } = await exec(`datasphere spaces users read --space ${space} --accept application/vnd.sap.datasphere.space.users.list+json`);
-            const users = JSON.parse(stdout);
-            const spaceUsers = [];
+          const { stdout } = await exec(
+            `datasphere spaces users read --space ${space} --accept application/vnd.sap.datasphere.space.users.list+json`,
+          );
+          const users = JSON.parse(stdout);
+          const spaceUsers = [];
 
-            for (const user of users) {
-                spaceUsers.push({
-                    space: space,
-                    user: user,
-                    updatedAt: currentTimestamp,
-                });
-            }
+          for (const user of users) {
+            spaceUsers.push({
+              space: space,
+              user: user,
+              updatedAt: currentTimestamp,
+            });
+          }
 
-            return spaceUsers;
-        })
-  );
+          return spaceUsers;
+        }),
+      );
 
-  return spacesUsers.flat();
-    } catch(err) {
-        Logger.error(`Error: ${err}`);
+      return spacesUsers.flat();
+    } catch (err) {
+      Logger.error(`Error: ${err}`);
     }
- }
+  }
 
- async getUserRoles(): Promise<UserRoles[]> {
+  async getUserRoles(): Promise<UserRoles[]> {
     try {
-        const currentTimestamp = new Date(Date.now()).toISOString();
+      const currentTimestamp = new Date(Date.now()).toISOString();
 
-        const { stdout } = await exec(`datasphere users list --accept application/vnd.sap.datasphere.space.users.details+json`);
-      
-        const userRolesFull = JSON.parse(stdout);
-      
-        return userRolesFull.map(userData => {
-            const userRoles = [];
-            const roles = userData.roles.split(';');
-      
-            for (const role of roles) {
-                userRoles.push(
-                    {
-                        user: userData.userName,
-                        role,
-                        updatedAt: currentTimestamp
-                    }
-                )
-            }
-      
-            return userRoles;
-        }).flat();  
-      } catch(err) {
-        Logger.error(`Error: ${err}`);
-      }
- }
+      const { stdout } = await exec(
+        `datasphere users list --accept application/vnd.sap.datasphere.space.users.details+json`,
+      );
 
- createCsv<T>(data: T, name: string): void {
-  new ObjectsToCsv(data).toDisk(`./downloads/${name}.csv`)
- }
+      const userRolesFull = JSON.parse(stdout);
 
- async createCsvs(): Promise<void> {
+      return userRolesFull
+        .map((userData) => {
+          const userRoles = [];
+          const roles = userData.roles.split(';');
+
+          for (const role of roles) {
+            userRoles.push({
+              user: userData.userName,
+              role,
+              updatedAt: currentTimestamp,
+            });
+          }
+
+          return userRoles;
+        })
+        .flat();
+    } catch (err) {
+      Logger.error(`Error: ${err}`);
+    }
+  }
+
+  async createCsv<T>(data: T[], name: string): Promise<void> {
+    const header = [];
+
+    for (const key in data[0]) {
+      header.push({ id: key, title: key.toUpperCase() });
+    }
+
+    const cvsWriter = createObjectCsvWriter({
+      path: `./downloads/${name}.csv`,
+      header,
+    });
+
+    await cvsWriter.writeRecords(data);
+  }
+
+  async createCsvs(): Promise<void> {
     try {
-        const spaces = await this.getSpacesList();
-        const spaceUsers = await this.getSpacesUsers(spaces);
-        const userRoles = await this.getUserRoles();
-    
-        await this.createCsv(spaceUsers, 'spaceUsers');
-        await this.createCsv(userRoles, 'userRoles');
-    
-        Logger.log('User data downloaded');
-    
-    } catch(err) {
-        Logger.error(`Error: ${err}`);
+      const spaces = await this.getSpacesList();
+      const spaceUsers = await this.getSpacesUsers(spaces);
+      const userRoles = await this.getUserRoles();
+
+      await this.createCsv(spaceUsers, 'spaceUsers');
+      await this.createCsv(userRoles, 'userRoles');
+
+      Logger.log('User data downloaded');
+    } catch (err) {
+      Logger.error(`Error: ${err}`);
     }
   }
 }
